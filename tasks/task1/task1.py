0c72af23f0442e01ffec70942fc5c8bdfdd70ba4
paintingpromisesss <paintingpromisesss@gmail.com>
2026-05-05 14:33:00 +0300

    now using q just like methodichka wants

--- tasks/task1/task1.py
+++ tasks/task1/task1.py
@@ -1,46 +1,58 @@
-from utils import EPS, phi, f, df
+from utils import EPS, dphi, phi, f, df
 
-def simple_iteration(x0, eps=EPS, max_iter=1000):
+def simple_iteration(x0, a, b, eps=EPS, max_iter=1000):
     if eps <= 0:
         raise ValueError("Эпсилон должен быть положительным")
     
+    q = abs(dphi(a))
+
+    if q >= 1:
+        print("Метод простых итераций не гарантирует сходимость (q >= 1)")
+
     x_prev = x0
 
     for k in range(1, max_iter + 1):
         x_next = phi(x_prev)
         diff = abs(x_next - x_prev)
 
-        if diff < eps:
+        error_estimate = q / (1 - q) * diff
+
+        if error_estimate <= eps:
             return x_next, k
-        
+
         x_prev = x_next
-    raise ValueError("Метод простых итераций не сошелся за максимальное количество итераций")
+
+    raise ValueError(
+        "Метод простых итераций не сошелся за максимальное количество итераций")
 
 
 def newton_method(x0, eps=EPS, max_iter=1000):
     if eps <= 0:
         raise ValueError("Эпсилон должен быть положительным")
-    
+
     x = x0
     for k in range(1, max_iter + 1):
         fx = f(x)
         dfx = df(x)
 
         if abs(dfx) < eps:
-            raise ValueError("Производная слишком мала, метод Ньютона может не сойтись")
-        
-        x_next = x - fx / dfx
-        dx = abs(x_next - x)
+            print("Производная слишком мала, метод Ньютона может не сойтись")
 
-        if dx < eps:
+        x_next = x - fx / dfx
+        diff = abs(x_next - x)
+
+        if diff < eps:
             return x_next, k
-        
+
         x = x_next
-    raise ValueError("Метод Ньютона не сошелся за максимальное количество итераций")
+    raise ValueError(
+        "Метод Ньютона не сошелся за максимальное количество итераций")
+
 
 def main():
     x0 = 1.1
-    root, iterations = simple_iteration(x0, max_iter=1000)
+    a, b = 1.1, 1.2
+    root, iterations = simple_iteration(x0, a, b, max_iter=1000)
 
     print("Метод простых итераций:")
     print(f"Приближенное решение: {root:.10f}")
@@ -55,6 +67,7 @@ def main():
     print(f"Количество итераций: {iterations}")
     print("Проверка:")
     print(f"f({root:.10f}) = {f(root):.10f}")
+
 
 if __name__ == "__main__":
     main()
--- tasks/task1/utils.py
+++ tasks/task1/utils.py
@@ -13,10 +13,22 @@ def phi(x):
     return value**0.25
 
 
+def dphi(x):
+    if x <= -2:
+        raise ValueError("x должен быть больше -2")
+    value = log(x+2) + 0.5
+
+    if value < 0:
+        raise ValueError("phi(x) должен быть неотрицательным")
+
+    return 1 / (4 * (x + 2) * value ** 0.75)
+
+
 def f(x):
     if x <= -2:
         raise ValueError("x должен быть больше -2")
     return log(x+2) - x**4 + 0.5
+
 
 def df(x):
     if x <= -2:
